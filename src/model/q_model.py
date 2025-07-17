821940cafcc0de012329c24dbfc6481b18c45e1a
Razvan <rzvn1805@gmail.com>
2025-07-17 11:29:03 +0300

    adjust(model): remove the dropout layers in the model, decrease epsilon decay and increase epsilon end. Test the final performance of the new model in the notebook.

--- src/model/q_model.py
+++ src/model/q_model.py
@@ -10,11 +10,8 @@ def build_q_model(input_shape=(3,), num_actions=2):
     model = keras.Sequential([
         keras.layers.Input(shape=input_shape),
         keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dropout(0.3),
         keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dropout(0.3),
         keras.layers.Dense(128, activation='relu'),
-        keras.layers.Dropout(0.3),
         keras.layers.Dense(num_actions)
     ])
     return model
